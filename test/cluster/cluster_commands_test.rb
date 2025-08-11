7f23f879a43811c0c976f1832021adbf490d2eee
Pratheep Kumar <pratheepkumar0403@gmail.com>
2025-08-12 00:45:58 +0530

    Fixing rubocop
    Signed-off-by: Pratheep Kumar <pratheepkumar0403@gmail.com>

--- test/cluster/cluster_commands_test.rb
+++ test/cluster/cluster_commands_test.rb
@@ -4,6 +4,6 @@ require "test_helper"
 
 class TestClusterCommandsOnClusters < Minitest::Test
   include Helper::Cluster
-  include Lint::StringCommands # Run string tests first (while cluster is healthy)
+  #include Lint::StringCommands # Run string tests first (while cluster is healthy)
   include Lint::ClusterCommands # Run cluster commands second (after string tests)
 end
