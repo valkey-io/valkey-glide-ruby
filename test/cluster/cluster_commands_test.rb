64a53d625211d7591eefe7d403518db1e829b6f2
Pratheep Kumar <pratheepkumar0403@gmail.com>
2025-08-11 23:50:31 +0530

    fixing test cases
    Signed-off-by: Pratheep Kumar <pratheepkumar0403@gmail.com>

--- test/cluster/cluster_commands_test.rb
+++ test/cluster/cluster_commands_test.rb
@@ -4,6 +4,6 @@ require "test_helper"
 
 class TestClusterCommandsOnClusters < Minitest::Test
   include Helper::Cluster
-  include Lint::ClusterCommands  # Run cluster tests first (non-destructive)
   include Lint::StringCommands   # Run string tests last (after cluster is broken)
+  include Lint::ClusterCommands  # Run cluster tests first (non-destructive)
 end
